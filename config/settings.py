3d2e6f06ffceae2e96b90d7e08df7185fe185901
Jialin Liu <mrliujalo@foxmail.com>
2024-06-20 17:58:29 +0800

    设置跨域 cors-admin

--- config/settings.py
+++ config/settings.py
@@ -37,20 +37,26 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
     "rest_framework",
-    
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  # 跨域设置的中间件在commonmiddleware 前面
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:5173",
+# ]
+
+ALLOWED_HOSTS = []
 
 ROOT_URLCONF = "config.urls"
 
